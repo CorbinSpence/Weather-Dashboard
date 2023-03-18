0b75eb5810f79591166dec00e93c15c3d098ddb4
CorbinSpence <corbinspence16@gmail.com>
2023-03-18 13:37:08 -0400

    added localstorage implementation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,27 @@
 /*
 Weather api returns temp in kelvin
 */
-//This is for testing purposes. Contains a key to locally saved api search instance.
-const positiveTestResultKey = "positiveResultKey"
 
+
+const previousResultsKey = "previousResultsKey"
+var previousResults = JSON.parse(localStorage.getItem(previousResultsKey))
+if(previousResults === null){
+    previousResults = []
+    localStorage.setItem(previousResultsKey, JSON.stringify(previousResults))
+}
+
+const searchedCitiesKey = "searchedCitiesKey"
+var searchedCities = JSON.parse(localStorage.getItem(searchedCitiesKey))
+if(searchedCities === null){
+    searchedCities = []
+    localStorage.setItem(searchedCitiesKey, JSON.stringify(searchedCities))
+}
+
+var searchedCity = "atlanta"
 var weatherURL = 'http://api.openweathermap.org/data/2.5/forecast'
-var geoCoderURL = 'http://api.openweathermap.org/geo/1.0/direct?q=Atlanta&limit=1&appid=62818af10b1b70f3c94825ea2a5873f2'
+var geoCoderURL = 'http://api.openweathermap.org/geo/1.0/direct?q='+searchedCity+'&limit=1&appid=62818af10b1b70f3c94825ea2a5873f2'
 var lat = 0
 var lon = 0
-var currentWeather = {
-    temp: '',
-    wind: '',
-    humidity: '',
-}
 
 function convertKelvinToFahrinheit(tempK){
     let far = (tempK-273.15) * 9/5 + 32
@@ -39,25 +48,36 @@ async function getWeatherReport(baseURL){
         cashe: 'default',
     })
     var response = await raw.json()
-    localStorage.setItem(positiveTestResultKey, JSON.stringify(response))
-    console.log(response)
+    let city = response.city.name
+    if(searchedCities.includes(city)){
+        return
+    }
+    let list = response.list
+    let currentStats = {"temp":convertKelvinToFahrinheit(list[0].main.temp), "wind":list[0].wind.speed, "humidity":list[0].main.humidity}
+    let fiveDayStats = []
+    let dayIndex = 1
+    for(l in list){
+        //console.log(list[l].dt)
+        //console.log(dayjs(list[l].dt*1000))
+        if(dayjs(list[l].dt*1000).isSame(dayjs(list[0].dt*1000).add(dayIndex, "day"))){
+            dayIndex++
+            fiveDayStats.push({"temp":convertKelvinToFahrinheit(list[l].main.temp), "wind":list[l].wind.speed, "humidity":list[l].main.humidity})
+            console.log(dayjs(list[l].dt*1000).format('D-H'))
+        }
+    }
+    let data = {"city":city, "currentStats":currentStats, "fiveDayStats":fiveDayStats}
+    console.log(previousResults)
+    previousResults.push(data)
+    searchedCities.push(city)
+    localStorage.setItem(previousResultsKey, JSON.stringify(previousResults))
+    localStorage.setItem(searchedCitiesKey, JSON.stringify(searchedCities))
+    console.log(data)
 }
-// if(localStorage.getItem(positiveTestResultKey)===null){
-//     getWeatherReport('http://api.openweathermap.org/data/2.5/forecast')
-// }
 
-// getCordinates(geoCoderURL)
-// console.log(lat)
-// console.log(lon)
 
-//getWeatherReport(weatherURL)
-var example = JSON.parse(localStorage.getItem(positiveTestResultKey))
-console.log(example)
-var exampleDataList = example.list
-console.log(exampleDataList)
-for(i in exampleDataList){
-    console.log(i)
-    let dt = exampleDataList[i].dt
-    let hour = dayjs(dt)
-    console.log(hour)
-}
+getCordinates(geoCoderURL)
+console.log(lat)
+console.log(lon)
+
+getWeatherReport(weatherURL)
+
