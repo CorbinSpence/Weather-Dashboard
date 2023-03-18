3293498b7b53aa3ce6db63caeb193096f23c3290
CorbinSpence <corbinspence16@gmail.com>
2023-03-18 13:54:58 -0400

    added code to dynamically add city buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ if(searchedCities === null){
     localStorage.setItem(searchedCitiesKey, JSON.stringify(searchedCities))
 }
 
-var searchedCity = "atlanta"
+var searchedCity = "boston"
 var weatherURL = 'http://api.openweathermap.org/data/2.5/forecast'
 var geoCoderURL = 'http://api.openweathermap.org/geo/1.0/direct?q='+searchedCity+'&limit=1&appid=62818af10b1b70f3c94825ea2a5873f2'
 var lat = 0
@@ -49,29 +49,33 @@ async function getWeatherReport(baseURL){
     })
     var response = await raw.json()
     let city = response.city.name
-    if(searchedCities.includes(city)){
-        return
-    }
-    let list = response.list
-    let currentStats = {"temp":convertKelvinToFahrinheit(list[0].main.temp), "wind":list[0].wind.speed, "humidity":list[0].main.humidity}
-    let fiveDayStats = []
-    let dayIndex = 1
-    for(l in list){
-        //console.log(list[l].dt)
-        //console.log(dayjs(list[l].dt*1000))
-        if(dayjs(list[l].dt*1000).isSame(dayjs(list[0].dt*1000).add(dayIndex, "day"))){
-            dayIndex++
-            fiveDayStats.push({"temp":convertKelvinToFahrinheit(list[l].main.temp), "wind":list[l].wind.speed, "humidity":list[l].main.humidity})
-            console.log(dayjs(list[l].dt*1000).format('D-H'))
+    if(!searchedCities.includes(city)){
+        let list = response.list
+        let currentStats = {"temp":convertKelvinToFahrinheit(list[0].main.temp), "wind":list[0].wind.speed, "humidity":list[0].main.humidity}
+        let fiveDayStats = []
+        let dayIndex = 1
+        for(l in list){
+            //console.log(list[l].dt)
+            //console.log(dayjs(list[l].dt*1000))
+            if(dayjs(list[l].dt*1000).isSame(dayjs(list[0].dt*1000).add(dayIndex, "day"))){
+                dayIndex++
+                fiveDayStats.push({"temp":convertKelvinToFahrinheit(list[l].main.temp), "wind":list[l].wind.speed, "humidity":list[l].main.humidity})
+                console.log(dayjs(list[l].dt*1000).format('D-H'))
+            }
         }
+        let data = {"city":city, "currentStats":currentStats, "fiveDayStats":fiveDayStats}
+        console.log(previousResults)
+        previousResults.push(data)
+        searchedCities.push(city)
+        localStorage.setItem(previousResultsKey, JSON.stringify(previousResults))
+        localStorage.setItem(searchedCitiesKey, JSON.stringify(searchedCities))
+        console.log(data)
     }
-    let data = {"city":city, "currentStats":currentStats, "fiveDayStats":fiveDayStats}
-    console.log(previousResults)
-    previousResults.push(data)
-    searchedCities.push(city)
-    localStorage.setItem(previousResultsKey, JSON.stringify(previousResults))
-    localStorage.setItem(searchedCitiesKey, JSON.stringify(searchedCities))
-    console.log(data)
+    let cityButtons = $('#city-list')
+    cityButtons.children().remove()
+    for(i in searchedCities){
+        cityButtons.append('<input type="button" value="'+searchedCities[i]+'" id="'+searchedCities[i]+'" class="bg-gray-400 rounded hover:bg-gray-300 mb-1">')
+    }
 }
 
 
