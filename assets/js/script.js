e5bbe12efe51e2f3ad3f7cb5058f5ff363779bf7
CorbinSpence <corbinspence16@gmail.com>
2023-03-19 15:56:51 -0400

    added previous city info button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,18 +50,20 @@ async function getWeatherReport(baseURL){
         cashe: 'default',
     })
     var response = await raw.json()
+    console.log(response.list)
     let city = response.city.name
     
     let list = response.list
-    let currentStats = {"temp":convertKelvinToFahrinheit(list[0].main.temp), "wind":list[0].wind.speed, "humidity":list[0].main.humidity}
+    let currentStats = {"time": dayjs(list[0].dt*1000).format('(MM-DD-YYYY)'), "temp":convertKelvinToFahrinheit(list[0].main.temp), "wind":list[0].wind.speed, "humidity":list[0].main.humidity}
     let fiveDayStats = []
-    let dayIndex = 1
     for(l in list){
         //console.log(list[l].dt)
         //console.log(dayjs(list[l].dt*1000))
-        if(dayjs(list[l].dt*1000).isSame(dayjs(list[0].dt*1000).add(dayIndex, "day"))){
-            dayIndex++
-            fiveDayStats.push({"temp":convertKelvinToFahrinheit(list[l].main.temp), "wind":list[l].wind.speed, "humidity":list[l].main.humidity})
+        if(parseInt(l) === 0){
+            continue
+        }
+        if(dayjs(list[l].dt*1000).hour() === 11){
+            fiveDayStats.push({"time": dayjs(list[l].dt*1000).format('(MM-DD-YYYY)'), "temp":convertKelvinToFahrinheit(list[l].main.temp), "wind":list[l].wind.speed, "humidity":list[l].main.humidity})
             console.log(dayjs(list[l].dt*1000).format('D-H'))
         }
     }
@@ -72,33 +74,60 @@ async function getWeatherReport(baseURL){
         searchedCities.push(city)
         localStorage.setItem(previousResultsKey, JSON.stringify(previousResults))
         localStorage.setItem(searchedCitiesKey, JSON.stringify(searchedCities))
-        console.log(data)
     }
+    console.log(data)
     let cityButtons = $('#city-list')
     cityButtons.children().remove()
+    // resets city buttons
     for(i in searchedCities){
         cityButtons.append('<input type="button" value="'+searchedCities[i]+'" id="'+searchedCities[i]+'" class="bg-gray-400 rounded hover:bg-gray-300 mb-1 city-button">')
     }
+    // sets current weather
     let currentForcast = $('#city-current-forcast')
     console.log(currentForcast.children('.temp'))
-    currentForcast.children('h2').text(city+' '+dayjs(list[0].dt*1000).format('(MM-DD-YYYY)'))
+    currentForcast.children('h2').text(city+' '+currentStats.time)
     currentForcast.children('.temp').text('Temp: '+currentStats.temp)
     currentForcast.children('.wind').text('Wind: '+currentStats.wind)
     currentForcast.children('.humidity').text('Humidity: '+currentStats.humidity)
+    // sets 5-day weather
     for(i in fiveDayStats){
-        
         let index = 1+parseInt(i)
         let temp = $('#'+index+'-days-after')
         console.log('#'+index+'-days-after')
         console.log(typeof i)
-        temp.children('h4').text(dayjs(list[0].dt*1000).add(index, "day").format('MM/DD/YYYY'))
+        temp.children('h4').text(fiveDayStats[i].time)
         temp.children('.temp').text('Temp: '+fiveDayStats[i].temp)
         temp.children('.wind').text('Wind: '+fiveDayStats[i].wind)
         temp.children('.humidity').text('Humidity: '+fiveDayStats[i].humidity)
     }
 }
 
-
+function displayWeather(city){
+    if(!searchedCities.includes(city)){
+        return
+    }
+    let data = JSON.parse(localStorage.getItem(previousResultsKey))
+    data = data[searchedCities.indexOf(city)]
+    
+    // sets current weather
+    let currentForcast = $('#city-current-forcast')
+    console.log(currentForcast.children('.temp'))
+    currentForcast.children('h2').text(city+' '+data.currentStats.time)
+    currentForcast.children('.temp').text('Temp: '+data.currentStats.temp)
+    currentForcast.children('.wind').text('Wind: '+data.currentStats.wind)
+    currentForcast.children('.humidity').text('Humidity: '+data.currentStats.humidity)
+    // sets 5-day weather
+    for(i in data.fiveDayStats){
+        let index = 1+parseInt(i)
+        let temp = $('#'+index+'-days-after')
+        console.log('#'+index+'-days-after')
+        console.log(typeof i)
+        temp.children('h4').text(data.fiveDayStats[i].time)
+        temp.children('.temp').text('Temp: '+data.fiveDayStats[i].temp)
+        temp.children('.wind').text('Wind: '+data.fiveDayStats[i].wind)
+        temp.children('.humidity').text('Humidity: '+data.fiveDayStats[i].humidity)
+    }
+}
 
 const searchCity = function (event){
     event.preventDefault()
@@ -111,6 +140,7 @@ const showPreviousCity = function (event){
     event.preventDefault()
     let city = $(event.target).val()
     console.log("setting up "+city)
+    displayWeather(city)
 }
 const submitButton = $('#submit-search')
 submitButton.on('click', searchCity)
