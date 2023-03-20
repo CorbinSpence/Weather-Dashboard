91b0e5c04067bbaf545fde15fd60f7c7a7e123dd
CorbinSpence <corbinspence16@gmail.com>
2023-03-19 23:20:13 -0400

    correctly implemented tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.{html, js, json}',
+    './assets/js/*.js'
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
